21f5179496158556432fb14ed6c95356b62e795c
samanta subedi <samantasubedi333@gmail.com>
2025-07-15 00:09:55 +0545

    input handling and data extraction from file

--- app/Inputpage.tsx
+++ app/Inputpage.tsx
@@ -1,4 +1,26 @@
+"use client";
+import { useState } from "react";
 const Inputpage = () => {
+  const [file, setfile] = useState<File | null>(null); // this is the way to define the type of state variable, enclose the type in <>
+  function handlefilechange(event: React.ChangeEvent<HTMLInputElement>) {
+    const selectedfile = event.target.files?.[0]; // .files?.[0]  means if the file exists get the first file
+    if (selectedfile) {
+      setfile(selectedfile);
+    }
+  }
+  const handleanalyze = () => {
+    if (!file) {
+      alert("please select a file first");
+      return;
+    }
+    //here we are extracting the data from the file and and sending it to backend , to send it to backend we need a backend api
+    const reader = new FileReader(); //This creates a new object from the built-in FileReader class (provided by the browser) and stores it in reader variable
+
+    reader.onload = async () => {
+      //“When the file has finished loading (reading), run this function.”
+      const filecontent = reader.result; //the result property of reader object contains actual file content that was read
+    };
+  };
   return (
     <div className="flex justify-center mt-[5%]">
       <div className="flex flex-col gap-10">
@@ -6,11 +28,15 @@ const Inputpage = () => {
           Drop your Resume here
         </div>
         <input
+          onChange={() => handlefilechange}
           className="p-4 rounded-2xl size-[300px] bg-slate-700"
           type="file"
           accept=".pdf,.docx"
         ></input>
-        <button className="btn btn-primary "> Analyze</button>
+        <button onClick={() => handleanalyze} className="btn btn-primary ">
+          {" "}
+          Analyze
+        </button>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+import OpenAI from "openai";
 import Inputpage from "./Inputpage";
 function Homepage() {
+  const token = process.env["GITHUB_TOKEN"];
+  const endpoint = "https://models.github.ai/inference";
+  const modelName = "openai/gpt-4o";
+
+  async function main() {
+    const client = new OpenAI({ baseURL: endpoint, apiKey: token });
+
+    const response = await client.chat.completions.create({
+      messages: [
+        { role: "system", content: "Analyze this resume" },
+        { role: "user", content: "input file appears here" },
+      ],
+      temperature: 1.0,
+      top_p: 1.0,
+      max_tokens: 1000,
+      model: modelName,
+    });
+
+    console.log(response.choices[0].message.content);
+  }
+
+  main().catch((err) => {
+    console.error("The sample encountered an error:", err);
+  });
+
   return (
     <>
       <div className="w-screen h-screen bg-gradient-to-r from-gray-400 to-gray-500">
